d1f6758906291c1c50b59112cef71862a78def17
NextGen Bot Network <nextgenbotnetwork@gmail.com>
2026-07-14 11:46:18 +0600

    Create package.js

---
+++ package.js
@@ -0,0 +1,38 @@
+{
+  "name": "fca-rahat",
+  "version": "3.0.1",
+  "description": "Facebook Chat API - fca-rahat by Rahat",
+  "main": "index.js",
+  "author": "Rahat",
+  "license": "MIT",
+  "keywords": [
+    "facebook",
+    "chat",
+    "api",
+    "fca",
+    "rahat",
+    "messenger",
+    "bot"
+  ],
+  "files": [
+    "index.js",
+    "utils.js",
+    "src/"
+  ],
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/nextgenbotnetwork/fca-rahat.git
+  },
+  "bugs": {
+    "url": "https://github.com/nextgenbotnetwork/fca-rahat/issues"
+  },
+  "homepage": "https://github.com/nextgenbotnetwork/fca-rahat#readme",
+  "dependencies": {
+    "chalk": "^4.1.2",
+    "gradient-string": "^2.0.2",
+    "mqtt": "^4.3.7",
+    "node-cron": "^3.0.2",
+    "request": "^2.88.2",
+    "ws": "^8.13.0"
+  }
+}
